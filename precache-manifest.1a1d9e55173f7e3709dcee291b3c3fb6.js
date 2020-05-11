b5720afa5c3ffc655f6e0276e698fccc1a07b7b0
MakarCodes <jarek.strupek@wp.pl>
2020-05-11 12:15:59 +0200

    Updates

---
+++ precache-manifest.1a1d9e55173f7e3709dcee291b3c3fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fefdfa144ff5d5321cd73b98f5d3c7e",
+    "url": "/Personal_Plan_App/index.html"
+  },
+  {
+    "revision": "31822889a9113c4678e9",
+    "url": "/Personal_Plan_App/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "9b6c159be9509079128f",
+    "url": "/Personal_Plan_App/static/js/2.f0d8e2c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Personal_Plan_App/static/js/2.f0d8e2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31822889a9113c4678e9",
+    "url": "/Personal_Plan_App/static/js/main.7f4a83d3.chunk.js"
+  },
+  {
+    "revision": "4d0a4da364eb151eadf7",
+    "url": "/Personal_Plan_App/static/js/runtime-main.71fc2d89.js"
+  }
+]);
